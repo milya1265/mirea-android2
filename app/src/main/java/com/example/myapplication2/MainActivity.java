d9284841e2130fd7ffecbfcecd67c6cfd0fa57e8
Данил <milya1265@mail.ru>
2024-02-24 11:58:20 +0300

    second commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myapplication2.databinding.ActivityMainBinding;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String tag = "GINGER";
+    private ActivityMainBinding binding; // Объект для доступа к представлениям
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+//        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.register_button);
+        Log.i(tag, "starting main activity");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(tag, "Click button login");
+                if ((binding.loginEditText.getText().toString().equals("pipi")) &&
+                        (binding.passwordEditText.getText().toString().equals("pupu"))){
+                    Toast.makeText(getApplicationContext(), "you'r logged in", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, AfterRegister.class);
+                    finish();
+                    startActivity(intent);
+                }
+                binding.loginEditText.setText("");
+                binding.passwordEditText.setText("");
+            }
+        });
+
+        TextView register = findViewById(R.id.register_text);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(tag, "Click text register");
+                EditText editText = findViewById(R.id.login_edit_text);
+                String login = editText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                intent.putExtra("login", login);
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication2/RegisterActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication2.databinding.ActivityRegisterBinding;
+
+import java.time.Instant;
+
+public class RegisterActivity extends AppCompatActivity {
+    String tag = "GINGER";
+
+    private ActivityRegisterBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // Установка макета
+
+        Bundle arguments = getIntent().getExtras();
+        if (arguments != null) {
+            String login = arguments.getString("login");
+            // Устанавливаем текст в EditText с помощью ViewBinding
+            binding.loginEditText.setText(login); // Устанавливаем текст
+        } else {
+            Log.e(tag, "No login information received");
+        }
+
+        binding.loginEditText.setHint("Enter login");
+
+        Log.i(tag, "starting second activity");
+
+
+        binding.registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+}
